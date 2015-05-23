8e7ee55927d0f80870d379d93fe9f9f011a55e24
qwang <wanqi962@gmail.com>
2015-05-23 21:42:55 +0200

    add matrix inversion
    cachematrix initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  minv <- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  calcminv <- function(solve) minv <<- solve
+  getminv <- function() minv
+  list(set = set, get = get,
+       calcminv = calcminv,
+       getminv = getminv)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  minv <- x$getminv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$calcminv(minv)
+  minv
+  
 }
